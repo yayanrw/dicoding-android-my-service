7b6bba45642cd385a5412813134f662c124676a6
yayan-msglow <yayanrahmat@msglowid.com>
2021-11-01 11:50:35 +0700

    add bound service

--- app/src/main/java/com/example/myservice/MainActivity.kt
+++ app/src/main/java/com/example/myservice/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var mServiceBound = false
+    private lateinit var mBoundService: MyBoundService
+
+    private val mServiceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName) {
+            mServiceBound = false
+        }
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val myBinder = service as MyBoundService.MyBinder
+            mBoundService = myBinder.getService
+            mServiceBound = true
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +41,19 @@ class MainActivity : AppCompatActivity() {
         }
         val btnStartBoundService = findViewById<Button>(R.id.btn_start_bound_service)
         btnStartBoundService.setOnClickListener {
+            val mBoundServiceIntent = Intent(this, MyBoundService::class.java)
+            bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE)
         }
         val btnStopBoundService = findViewById<Button>(R.id.btn_stop_bound_service)
         btnStopBoundService.setOnClickListener {
+            unbindService(mServiceConnection)
+        }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mServiceBound) {
+            unbindService(mServiceConnection)
         }
     }
 }
